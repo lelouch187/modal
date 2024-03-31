a7df06190731548b403a4331d253bb41164fe9f8
lelouch187 <vanushs187@gmail.com>
2024-03-31 15:33:25 +0600

    add new bugs

--- modal.js
+++ modal.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const URL = 'https://jsonplaceholder.typicode.com/posts'
 
     const btnOpenModal = document.getElementById("modal_btn"),
+        modalContent = document.querySelector(".modal__content"),
         modal = document.querySelector(".modal"),
         btnCloseModal = document.querySelector(".modal__close"),
         form = document.querySelector(".modal__form"),
@@ -22,15 +23,40 @@ document.addEventListener("DOMContentLoaded", () => {
 
     btnCloseModal.addEventListener("click", closeModal)
 
-    //отправка запроса на сервер
+    //закрытие модалки при клике в любую часть экрана, кроме содержимого самой модалки
+    modal.addEventListener("click", closeModal)
+    modalContent.addEventListener("click", (e) => e.stopPropagation())
 
+    //закрытие модалки при нажатии на клавишу esc
+    document.addEventListener("keydown", function (e) {
+
+        if (e.keyCode === 27) {
+            closeModal()
+        }
+    });
+
+    //отправка запроса на сервер
     async function sendData(data) {
+        let mod_id = document.querySelector('div[id="mod_id"]');
+
         return await fetch(URL, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify({
+                ModID: mod_id,
+                Type: 10,  // 10 - Дубликат
+                Body: {
+                    DuplicateLink: data
+                }
+            })
+        }).then(response => {
+            return response.json()
+        }).then(data => {
+            return data
+        }).catch(error => {
+            alert(error.message)
         })
     }
 
@@ -42,9 +68,14 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         const response = await sendData(inputValue)
-
+        //выводим результат
         alert(response)
+
+        //закрываем модалку после ответа
+        closeModal()
     }
 
     form.addEventListener("submit", onSubmit)
 })
+
+
